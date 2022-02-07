5e91b41a74c4ab41dc635216742d97eb50445b26
Harry45 <arrykrish@gmail.com>
2022-02-07 14:30:58 +0000

    main file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import src.processing as sp
 
-sp.generate_random_set(['spiral', 'ring'], 1000, save=True)
+# sp.generate_random_set(['spiral', 'ring'], 1000, save=True)
 
-d = sp.split_data(['spiral', 'ring'], val_size=0.35, save=True)
+# d = sp.split_data(['spiral', 'ring'], val_size=0.35, save=True)
+
+sp.images_train_validate(['spiral', 'ring'])
