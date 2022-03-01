00d05309969337ab9994423638b24099c3a00e0c
Arrykrishna Mootoovaloo <arrykrish@gmail.com>
2022-03-01 10:25:14 +0000

    Submission Script

--- jobs/script.sh
+++ jobs/script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --mail-type=BEGIN,END 
+#SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=arrykrish@gmail.com
-#SBATCH --time=00:10:00
-#SBATCH --job-name=deeplearning_galaxyzoo
+#SBATCH --time=00:20:00
+#SBATCH --job-name=siamese_network
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=8G
 #SBATCH --partition=short
@@ -11,7 +11,7 @@
 
 cd $SCRATCH || exit 1
 
-rsync -av $HOME/example-torch/test_plot.py ./
+rsync -r $HOME/GAZFEW/ ./
 
 module purge
 module load CUDA/11.3.1
@@ -19,8 +19,6 @@ module load Anaconda3
 export CONPREFIX=$DATA/pytorch-env39
 source activate $CONPREFIX
 
-python test_plot.py
+python train.py
 
-nvidia-smi > nvidia.txt 
-
-rsync -av --exclude=test_plot.py ./ $HOME/example-torch/outputs/
+rsync -r ./outputs/ $HOME/GAZFEW/
