ac8507253fd262aeff843b0b9b003da18d07e25b
Harry45 <arrykrish@gmail.com>
2022-03-01 19:24:56 +0000

    submission script

--- jobs/script.sh
+++ jobs/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=arrykrish@gmail.com
-#SBATCH --time=00:20:00
+#SBATCH --time=00:50:00
 #SBATCH --job-name=siamese_network
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=8G
